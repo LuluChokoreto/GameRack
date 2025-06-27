c59e02c8cc2dfe055824bedc21d17f159ae7d995
Alfredogames26123 <alfredogames26123@gmail.com>
2025-06-27 13:23:38 +0200

    fix

--- Back/server.js
+++ Back/server.js
@@ -8,11 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/', require('./controllers/index.js'));
 
-<<<<<<< HEAD
-app.listen(process.env.SERVERPORT, () => {
-    console.log(`Server is running on port ${process.env.SERVERPORT}`);
-}) 
-=======
 sequelize.sync().then(() => {
     app.listen(process.env.SERVERPORT, () => {
         console.log(`Server is running on port ${process.env.SERVERPORT}`);
@@ -21,4 +16,4 @@ sequelize.sync().then(() => {
 }).catch(err => {
     console.error("Erreur lors de la synchronisation des tables :", err);
 });
->>>>>>> origin/Ryan
+
